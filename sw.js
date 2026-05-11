9d32271adfaa99309cfda15e25032084f25744f5
class-dash-site <xiicmarygiri@gmail.com>
2026-05-11 21:28:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,18 @@ const ASSETS = [
   "./index.html",
   "./manifest.json",
   "./icon.png",
-  "./logo.png"
+  "./logo.png",
+  "./bg1.png",
+  "./bg2.png",
+  "./bg3.png",
+  "./bg4.png",
+  "./bg5.png",
+  "./bg6.png",
+  "./bg7.png",
+  "./bg8.png",
+  "./bg9.png",
+  "./bg10.png",
+  "./Ndot_font.woff2"  // worth adding this too
 ];
 
 self.addEventListener("install", event => {
